df83c500a2ccdc70aba6fa2a4d1db8abddf2becf
zmxsho <46037159+zmxsho@users.noreply.github.com>
2018-12-22 14:00:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix = "1"
 console.log("Scrpit By Dream");
 
+client.on('message', message => {
+if (message.content.startsWith(prefix + "say")) {
+var args = message.content.split(" ")
+args = args.join(" ").slice(args[0].length);
 
+if (!args) return;
+
+message.channel.send(args);
+};
+})
 client.on("ready", () => {
 let channel =     client.channels.get("525804669746806788")
 setInterval(function() {
